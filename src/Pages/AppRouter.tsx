2e66ac619fb2182dbeaabfd65e736f61943fe149
Erick Gomez <erick0gomez98@gmail.com>
2019-10-06 13:31:58 -0500

    Se agregan las páginas que se usaran

--- src/Pages/AppRouter.tsx
+++ src/Pages/AppRouter.tsx
@@ -1,12 +1,20 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import MainPage from './MainPage';
+import AdminPage from './AdminPage';
+import SalidaPage from './SalidaPage';
+import EntradaPage from './EntradaPage';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <main className='Main'>
-                <Route path='/'><MainPage /></Route>
+                <Switch>
+                    <Route path='/entrada' exact><EntradaPage /></Route>
+                    <Route path='/salida' exact><SalidaPage /></Route>
+                    <Route path='/admin' exact><AdminPage /></Route>
+                    <Route path='*'> <Redirect to='/admin' /> </Route>
+                </Switch>
             </main>
         </BrowserRouter>
     )
